e20d7264c0d0c63b18d596a022db135516a7d27c
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2024-07-15 15:31:21 +0100

    New admin email subject

--- handlerPut.ts
+++ handlerPut.ts
@@ -63,7 +63,7 @@ const handlerPut = async (request: Request, kv: Deno.Kv) => {
     resend.emails.send({
       from: "NN1 Dev Club <club@nn1.dev>",
       to: "club@nn1.dev",
-      subject: "New signup",
+      subject: "🎉 New signup",
       html: emailAdmin.html,
       text: emailAdmin.text,
     }),
