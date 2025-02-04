c1b30cc820db9e7f7551c090a8bc1a4571e0dc3a
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2025-02-04 21:20:44 +0000

    send email on ticket cancellation

--- handlerDelete.ts
+++ handlerDelete.ts
@@ -1,4 +1,8 @@
+import { Resend } from "npm:resend";
 import { PREFIX } from "./constants.ts";
+import { renderEmailAdminSignupCancel } from "https://raw.githubusercontent.com/nn1-dev/emails/main/emails/admin-signup-cancel.tsx";
+
+const resend = new Resend(Deno.env.get("API_KEY_RESEND"));
 
 const handlerDelete = async (request: Request, kv: Deno.Kv) => {
   console.log(`✨ DELETE: ${request.url}`);
@@ -13,7 +17,7 @@ const handlerDelete = async (request: Request, kv: Deno.Kv) => {
     console.log(`✨ 400`);
     return Response.json(
       {
-        status: "success",
+        status: "error",
         statusCode: 400,
         data: null,
         error: "Invalid request.",
@@ -21,6 +25,38 @@ const handlerDelete = async (request: Request, kv: Deno.Kv) => {
       { status: 400 },
     );
   }
+
+  const ticket = await kv.get<KvEntryTicket>([
+    PREFIX,
+    Number(eventId),
+    ticketId,
+  ]);
+
+  if (!ticket.value) {
+    console.log(`✨ 400`);
+    return Response.json(
+      {
+        status: "error",
+        statusCode: 400,
+        data: null,
+        error: "Ticket does not exists.",
+      },
+      { status: 400 },
+    );
+  }
+
+  const email = renderEmailAdminSignupCancel({
+    name: ticket.value.name,
+    email: ticket.value.email,
+  });
+
+  resend.emails.send({
+    from: "NN1 Dev Club <club@nn1.dev>",
+    to: Deno.env.get("ADMIN_RECIPIENTS")?.split(",")!,
+    subject: "Ticket cancelled 👎",
+    html: email.html,
+    text: email.text,
+  });
 
   await kv.delete([PREFIX, Number(eventId), ticketId]);
 
