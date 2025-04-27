b5e903b7d00bacbcb534af6387c97ee1de62170a
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2025-04-27 16:52:51 +0100

    up

--- index.ts
+++ index.ts
@@ -22,7 +22,8 @@ Deno.serve(async (request) => {
   const auth = request.headers.get("Authorization");
 
   if (auth !== `Bearer ${Deno.env.get("API_KEY")}`) {
-    Sentry.captureException("Unauthorized API use");
+    Sentry.captureMessage("Unauthorized API use", "warning");
+
     return Response.json(
       {
         status: "error",
